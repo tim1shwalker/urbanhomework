ca145c893289f0694c086a289425ad44ce165c7b
tim1shwalker <mr.black3440@gmail.com>
2024-06-14 20:26:44 +0300

    Create module1hard.py
    Хард

---
+++ module1hard.py
@@ -0,0 +1,10 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+
+average_grades = {}
+
+for i, student in enumerate(students):
+    grades_sum = grades[i]
+    average_grade = sum(grades_sum) / len(grades_sum)
+    average_grades[student] = average_grade
+print(average_grades)
