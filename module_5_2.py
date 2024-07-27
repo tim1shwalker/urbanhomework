6211cce2e9651241aadcc4086e5e793cce90ac92
tim1shwalker <mr.black3440@gmail.com>
2024-07-27 18:15:52 +0300

    Create module_5_2.py

---
+++ module_5_2.py
@@ -0,0 +1,26 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor < 1 or new_floor > self.number_of_floors:
+            print("Такого этажа не существует")
+        else:
+            for floor in range(1, new_floor + 1):
+                print(floor)
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+
+# Пример использования класса
+my_house = House('ЖК Эльбрус', 30)
+
+print(len(my_house))
+print(my_house)
+my_house.go_to(5)
+my_house.go_to(31)
+my_house.go_to(0)
